2ee4f76584fdc09ec5e2b8b04e26a12a8f6fa535
Artur Kostiuk <artukos54@gmail.com>
2018-12-03 16:57:15 +0200

    ready_task_without_accordion

--- css-practice/task_3/index.js
+++ css-practice/task_3/index.js
@@ -3,6 +3,7 @@
 /* Accordion */
 const accArr = [...document.getElementsByClassName('accordion')];
 const accContent = [...document.getElementsByClassName('panel')];
+
 accArr.forEach((e) => {
   e.onclick = function() {
     if (!e.nextElementSibling.classList.contains('panel_open')) {
@@ -47,7 +48,7 @@ modal.onclick = function(e) {
 
 
 /* Link Tabs */
-let navLinksArr = [...document.getElementsByClassName('nav_link')];
+const navLinksArr = [...document.getElementsByClassName('nav_link')];
 navLinksArr.forEach((e) => {
   e.onclick = function() {
     clearLinks();
@@ -61,4 +62,35 @@ function clearLinks() {
   });
 }
 
+
 /* Slider */
+const arrowLeft = document.getElementById('slider_container_arrow_left');
+const arrowRight = document.getElementById('slider_container_arrow_right');
+const arrowLeftBottom = document.getElementById('slider_container_arrow_left_bottom');
+const arrowRightBottom = document.getElementById('slider_container_arrow_right_bottom');
+const contentSliderTop = document.getElementById('content');
+const contentSliderBottom = document.getElementById('content_bottom');
+let counter = 0;
+const nextSlide = (content) => {
+  if (counter === -66.66666) {
+    counter = 0;
+    content.style.transform = 'translateX(' + counter + '%)';
+  } else {
+    counter-=33.33333;
+    content.style.transform = 'translateX(' + counter + '%)';
+  }
+};
+const prevSlide = (content) => {
+  if (counter === 0) {
+    counter = -66.66666;
+    content.style.transform = 'translateX(' + counter + '%)';
+  } else {
+    counter += 33.33333;
+    content.style.transform = 'translateX(' + counter + '%)';
+  }
+};
+
+arrowRight.onclick = nextSlide.bind(null, contentSliderTop);
+arrowLeft.onclick = prevSlide.bind(null, contentSliderTop);
+arrowLeftBottom.onclick = prevSlide.bind(null, contentSliderBottom);
+arrowRightBottom.onclick = nextSlide.bind(null, contentSliderBottom);
